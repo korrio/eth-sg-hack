f4f43f24a97550df0643ee9b27150a2e35308320
Sakapong <sakapong.dong@gmail.com>
2023-09-11 16:22:43 +0800

    Add Metamask

--- web3/src/pages/_app.tsx
+++ web3/src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// import { AppProps } from 'next/app';
+// import type { ReactElement, ReactNode } from 'react';
+// import type { NextPage } from 'next';
+
+import Head from 'next/head';
+import { MetaMaskContextProvider } from '@/hooks/useMetaMask';
+
+// export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+//   getLayout?: (page: ReactElement) => ReactNode;
+// };
+//
+// type AppPropsWithLayout = AppProps & {
+//   Component: NextPageWithLayout;
+// };
+
+function MyApp({ Component, pageProps }: any) {
+  const getLayout = Component.getLayout ?? ((page: any) => page);
+
+  return (
+    <MetaMaskContextProvider>
+      <Head>
+        <title>DIVPAY</title>
+      </Head>
+      <div>{getLayout(<Component {...pageProps} />)}</div>
+    </MetaMaskContextProvider>
+  );
 }
+
+export default MyApp;
