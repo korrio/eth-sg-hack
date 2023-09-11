be0f1b252db0057eda27734f97a555378e3a5db9
siwakon.dev <sc.sivakorn@gmail.com>
2023-09-12 04:06:06 +0800

    add payment success, expired, trigger event payment

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -12,15 +12,43 @@ import {
 } from '@mui/material';
 import { TProduct } from '@/types/shop';
 import { Add, Remove } from '@mui/icons-material';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 type Props = {
   product: TProduct;
+  setSelectedAmount: any;
   onOpen: any;
 };
 
 const Product: React.FC<Props> = (props) => {
-  const { product, onOpen } = props;
+  const router = useRouter();
+  const { product, setSelectedAmount, onOpen } = props;
   const [amount, setAmount] = useState(1);
+
+  async function onClick() {
+    const res = await axios.post(
+      `https://apis.finnwork.co/crypto/wallet/newrequestdeposit`,
+      {
+        merchantcode: 'mc00000003',
+        cusid: 'txlineasgd',
+        asset: 'USDC',
+        network: 59140,
+        amount: amount,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'depay-x-key': `${process.env.NEXT_PUBLIC_API_KEY_SUPERADMIN}`,
+        },
+      }
+    );
+    console.log('res', res);
+    router.push({
+      pathname: `/payment/${res.data.id}`,
+    });
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -63,7 +91,7 @@ const Product: React.FC<Props> = (props) => {
               size="small"
               fullWidth
               variant="contained"
-              onClick={() => onOpen()}
+              onClick={() => onClick()}
             >
               Buy
             </Button>
--- frontend/src/pages/shop/[shopId].tsx
+++ frontend/src/pages/shop/[shopId].tsx
@@ -1,67 +1,23 @@
-import React, { ChangeEvent, ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import MainLayout from '@/layouts/MainLayout';
 import { useRouter } from 'next/router';
 import shopService from '@/services/shop';
 import { TProduct, TShop } from '@/types/shop';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
 import Product from '@/components/Product';
 import { Grid } from '@mui/material';
-import { useDebounce } from 'usehooks-ts';
 import Head from 'next/head';
 import QRCode from '@/components/QRCode';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 const ShopDetail = () => {
   const router = useRouter();
   const { shopId } = router.query;
+  const [selectedAmount, setSelectedAmount] = useState(1);
   const [open, setOpen] = useState(false);
+
   const [shop, setShop] = useState<TShop>({
     description: '',
     id: 0,
@@ -69,13 +25,6 @@ const ShopDetail = () => {
     name: '',
     products: [],
   });
-
-  const [value, setValue] = useState<string>('');
-  const debouncedValue = useDebounce<string>(value, 500);
-  console.log('debouncedValue', debouncedValue);
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
 
   async function fetchData() {
     const res = await shopService.getShopById(shopId);
@@ -98,7 +47,7 @@ const ShopDetail = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <QRCode open={open} onClose={onClose} />
+      {open && <QRCode open={open} onClose={onClose} amount={selectedAmount} />}
       <Head>
         <title>{shop.name}</title>
       </Head>
@@ -112,18 +61,18 @@ const ShopDetail = () => {
           >
             {shop.name}
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                handleChange(e);
-              }}
-            />
-          </Search>
+          {/* <Search> */}
+          {/*  <SearchIconWrapper> */}
+          {/*    <SearchIcon /> */}
+          {/*  </SearchIconWrapper> */}
+          {/*  <StyledInputBase */}
+          {/*    placeholder="Search…" */}
+          {/*    inputProps={{ 'aria-label': 'search' }} */}
+          {/*    onChange={(e: ChangeEvent<HTMLInputElement>) => { */}
+          {/*      handleChange(e); */}
+          {/*    }} */}
+          {/*  /> */}
+          {/* </Search> */}
         </Toolbar>
       </AppBar>
       <Box sx={{ padding: '1rem 0' }}>
@@ -131,7 +80,11 @@ const ShopDetail = () => {
           {shop.products.map((product: TProduct, index: React.Key) => {
             return (
               <Grid key={index} item xs={3}>
-                <Product product={product} onOpen={onOpen} />
+                <Product
+                  onOpen={onOpen}
+                  product={product}
+                  setSelectedAmount={setSelectedAmount}
+                />
               </Grid>
             );
           })}
