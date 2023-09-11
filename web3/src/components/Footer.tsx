391ad919b0a8d70d76ee1b7e67dec0c540e61c23
Sakapong <sakapong.dong@gmail.com>
2023-09-11 16:21:39 +0800

    Prepare template

---
+++ web3/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="pt-6 pb-12">
+      <div className="container mx-auto text-center">
+        <h3 className="text-2xl font-bold text-black">Join our community</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
