36956efd86183e2fc7d60f7cae4ee10cf1c315c3
NLGRF <non.devtech@gmail.com>
2023-12-08 17:38:36 +0700

    new sol

--- relay/main.py
+++ relay/main.py
@@ -30,7 +30,7 @@ def fetch_data(api_key_token, api_key):
     all_listRequestDeposits_data = listRequestDeposits_data_new + listRequestDeposits_data_watching
     # list to dataframe
     all_listRequestDeposits_data = pd.DataFrame(all_listRequestDeposits_data)
-    # print("all_listRequestDeposits_data", all_listRequestDeposits_data)
+    print("all_listRequestDeposits_data", all_listRequestDeposits_data)
     filtered_lists = []
     filtered_lists_expired = []
     # filter data by network if data is not empty list
@@ -40,12 +40,13 @@ def fetch_data(api_key_token, api_key):
         all_listRequestDeposits_data['reqtime'] = pd.to_datetime(all_listRequestDeposits_data['reqtime'])
         all_listRequestDeposits_data['reqtime'] = all_listRequestDeposits_data['reqtime'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
         all_listRequestDeposits_data['reqtime'] = pd.to_datetime(all_listRequestDeposits_data['reqtime'])
-
+        all_listRequestDeposits_data['reqtime'] = all_listRequestDeposits_data['reqtime'] + pd.Timedelta(hours=7)
         all_listRequestDeposits_data['endtime'] = all_listRequestDeposits_data['reqtime'] + pd.Timedelta(minutes=10)
+
         # time zone +7
-        all_listRequestDeposits_data['endtime'] = all_listRequestDeposits_data['endtime'] + pd.Timedelta(hours=7)
+        all_listRequestDeposits_data['reqtime'] = all_listRequestDeposits_data['reqtime'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
         all_listRequestDeposits_data['endtime'] = all_listRequestDeposits_data['endtime'].dt.strftime('%Y-%m-%d %H:%M:%S.%f')
-        print("all_listRequestDeposits_data__network", all_listRequestDeposits_data)
+        print("all_listRequestDeposits_data_network", all_listRequestDeposits_data)
         # data to list
         all_listRequestDeposits_data = all_listRequestDeposits_data.to_dict('records')
         filtered_lists = all_listRequestDeposits_data
@@ -59,13 +60,13 @@ def fetch_data(api_key_token, api_key):
                 filtered_list.append(i)
 
             if i['status'] == 'watching':
-                now = pd.to_datetime(pd.to_datetime('now').strftime('%Y-%m-%d %H:%M:%S.%f')) + pd.Timedelta(hours=7)
-                if np.where(now.strftime('%Y-%m-%d %H:%M:%S.%f') > i['endtime'], True, False):
+                now = (pd.to_datetime(pd.to_datetime('now').strftime('%Y-%m-%d %H:%M:%S.%f')) + pd.Timedelta(hours=7)).strftime('%Y-%m-%d %H:%M:%S.%f')
+                if np.where(now > i['endtime'], True, False):
                     div.updateWatchDeposit(api_key, i['id'], 'expired')
                 else:
                     filtered_list_watching.append(i)
                     if len(filtered_list_watching) != 0:
-                        while (np.where(pd.to_datetime('now').strftime('%Y-%m-%d %H:%M:%S.%f') < i['endtime'], True, False)):
+                        if (np.where(now < i['endtime'], True, False)):
                             time.sleep(3)
                             print("--------------------------------------------------------------------------------------------------------------")
                             print("I'm working...")
@@ -73,30 +74,31 @@ def fetch_data(api_key_token, api_key):
                             # print(filtered_list_watching[0])
                             # print("now", pd.to_datetime('now').strftime('%Y-%m-%d %H:%M:%S.%f') , "endtime", str(filtered_list_watching[0]['endtime']))
                             print("--------------------------------------------------------------------------------------------------------------")
-                            check_deposits = div.validate_transaction(filtered_list_watching[0]['assestaddress'], filtered_list_watching[0]['address'], filtered_list_watching[0]['reqblock'], api_key_token)
-                            print("check_deposits", check_deposits)
-                            print("check_deposits != []", check_deposits != [])
-                            print("check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied'", check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied')
-                            if check_deposits != [] and check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied':
-                                print("test", check_deposits != [] and check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied')
-                                print("Deposit detected")
-                                print(check_deposits)
-                                div.updateWatchDeposit(api_key, filtered_list_watching[0]['id'], 'watched')
-                                print("watched")
-                                print("merchantcode", filtered_list_watching[0]['merchantcode'])
-                                print("asset", check_deposits[0]['tokenSymbol'])
-                                print("network", filtered_list_watching[0]['network'])
-                                print("hookurl", filtered_list_watching[0]['hookurl'])
-                                if filtered_list_watching[0]['hookurl'] is None:
-                                    filtered_list_watching[0]['hookurl'] = ""
-                                print("hookurl", filtered_list_watching[0]['hookurl'])
-                                print("amount", check_deposits[0]['value'])
-                                print("trxhash", check_deposits[0]['hash'])
-                                print("status", 1)
-                                print("web3CommitDeposit")
-                                div.web3CommitDeposit(api_key, filtered_list_watching[0]['merchantcode'], filtered_list_watching[0]['cusid'], check_deposits[0]['tokenSymbol'], filtered_list_watching[0]['network'], filtered_list_watching[0]['hookurl'], check_deposits[0]['value'], check_deposits[0]['hash'], 1)
-                                filtered_list_watching.pop(0)
-                                print("web3CommitDeposit done")
+                            for iflw, flw in enumerate(filtered_list_watching):
+                                check_deposits = div.validate_transaction(flw['assestaddress'], flw['address'], flw['reqblock'], api_key_token)
+                                print("check_deposits", check_deposits)
+                                print("check_deposits != []", check_deposits != [])
+                                print("check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied'", check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied')
+                                if check_deposits != [] and check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied':
+                                    print("test", check_deposits != [] and check_deposits != 'Max rate limit reached, rate limit of 5/1sec applied')
+                                    print("Deposit detected")
+                                    print(check_deposits)
+                                    div.updateWatchDeposit(api_key, flw['id'], 'watched')
+                                    print("watched")
+                                    print("merchantcode", flw['merchantcode'])
+                                    print("asset", check_deposits[0]['tokenSymbol'])
+                                    print("network", flw['network'])
+                                    print("hookurl", flw['hookurl'])
+                                    if flw['hookurl'] is None:
+                                        flw['hookurl'] = ""
+                                    print("hookurl", flw['hookurl'])
+                                    print("amount", check_deposits[0]['value'])
+                                    print("trxhash", check_deposits[0]['hash'])
+                                    print("status", 1)
+                                    print("web3CommitDeposit")
+                                    div.web3CommitDeposit(api_key, flw['merchantcode'], flw['cusid'], check_deposits[0]['tokenSymbol'], flw['network'], flw['hookurl'], check_deposits[0]['value'], check_deposits[0]['hash'], 1)
+                                    filtered_list_watching.pop(iflw)
+                                    print("web3CommitDeposit done")
                     else:
                         print("filtered_list_watching", filtered_list_watching)
             else:
